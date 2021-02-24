b83bfdbb482ef6e17dcf8904109322833119ab34
immxalan <aedocoding@gmail.com>
2021-02-23 22:57:23 -0800

    Project finished, full CRUD for practice API

--- test.py
+++ test.py
@@ -8,11 +8,15 @@ data = [{"likes": 78, "name": "Joe", "views": 100000},
 for i in range(len(data)):
     response = requests.put(BASE + "video/" + str(i), data[i])
     print(response.json())
-    
-input()
-response = requests.delete(BASE + "video/0")
-print(response)
+
+# input()
+# response = requests.delete(BASE + "video/0")
+# print(response)
 
 input()
 response = requests.get(BASE + "video/2")
+print(response.json())
+
+input()
+response = requests.patch(BASE + "video/2", {"views": 99})
 print(response.json())
